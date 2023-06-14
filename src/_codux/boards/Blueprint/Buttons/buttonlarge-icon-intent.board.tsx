85a43f171d5bf4f222e0d02a6f94bffc384267fa
mikawix <mikan@wix.com>
2023-06-14 22:11:31 +0300

    danger button with icon

--- src/_codux/boards/Blueprint/Buttons/buttonlarge-icon-intent.board.tsx
+++ src/_codux/boards/Blueprint/Buttons/buttonlarge-icon-intent.board.tsx
@@ -1,16 +1,17 @@
 import { createBoard } from '@wixc3/react-board';
 import { Button } from '@blueprintjs/core';
+// board cover
+import cover from '../../../../assets/buttons/danger-left-icon.svg';
 
 export default createBoard({
-    name: 'Button 2',
+    name: 'Danger with icon',
     Board: () => (
         <div>
-            <Button
-                icon="refresh"
-                large={true}
-                intent="danger">
+            <Button icon="refresh" large={true} intent="danger">
                 Reset
             </Button>
         </div>
     ),
+    tags: ['button', 'danger'],
+    cover,
 });
