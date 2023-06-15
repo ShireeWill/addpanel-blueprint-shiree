eb2fe1e65edf9665c8265b48e41df16fddbd95bc
ShireeWill <shiree@wix.com>
2023-06-15 16:02:36 +0300

    progress bar fixed

---
+++ src/_codux/boards/Blueprint/Progress-Bar/progress-bar.board.tsx
@@ -0,0 +1,17 @@
+import { createBoard } from '@wixc3/react-board';
+import { H5, Intent, ProgressBar, Slider, Switch } from '@blueprintjs/core';
+
+// board cover
+import cover from '../../../../../src/assets/progress-bar/primary.svg';
+
+export default createBoard({
+    name: 'Progress bar',
+    Board: () => {
+        return <ProgressBar intent="primary" value={0.7} />;
+    },
+    environmentProps: {
+        canvasWidth: 500,
+    },
+    tags: ['progress', 'bar', 'primary'],
+    cover,
+});
