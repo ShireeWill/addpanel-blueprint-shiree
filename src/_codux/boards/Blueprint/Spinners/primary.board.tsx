7bdda7bdd1b980f467f9c8cced37dcc68d258535
ShireeWill <shiree@wix.com>
2023-06-15 16:20:38 +0300

    spinners

--- src/_codux/boards/Blueprint/Spinners/primary.board.tsx
+++ src/_codux/boards/Blueprint/Spinners/primary.board.tsx
@@ -1,8 +1,9 @@
 import { createBoard } from '@wixc3/react-board';
 import { Spinner, ISpinnerProps } from '@blueprintjs/core';
 
+
 export default createBoard({
-    name: 'Spinner',
+    name: 'Primary',
     Board: () => 
         <Spinner intent="primary" size={40}/>
 });
