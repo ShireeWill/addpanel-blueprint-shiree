742873840842703ba2cbbb0a6e756c77f85ec5e9
ShireeWill <shiree@wix.com>
2023-06-15 16:51:58 +0300

    tags cover added

--- src/_codux/boards/Blueprint/Tags/tags-with-icon.board.tsx
+++ src/_codux/boards/Blueprint/Tags/tags-with-icon.board.tsx
@@ -1,17 +1,23 @@
 import { createBoard } from '@wixc3/react-board';
 import { Button, H5, Intent, Switch, Tag, Icon } from "@blueprintjs/core";
 
-const INITIAL_TAGS = ["London", "New York", "San Francisco", "Seattle"];
+
+// board cover
+import cover from '../../../../src/assets/tags/tags-with-icon.svg';
+
+
+const INITIAL_TAGS = ["Seattle","London", "New York"];
 const tag="Hello"
 export default createBoard({
-    name: 'Tags',
+    name: 'Tags with Icon',
     Board: () => <>{
     INITIAL_TAGS.map(item => <Tag    
         icon={"home"}
     >
         {item}
     </Tag>)
-    }</>
+    }
+    </>
     
 });
 
