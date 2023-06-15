e021b52e77b2a4a27d7304b4441e8e38960126d6
mikawix <mikan@wix.com>
2023-06-15 14:51:14 +0300

    form group options

--- src/_codux/boards/Blueprint/Form-Controls/form-group-options.board.tsx
+++ src/_codux/boards/Blueprint/Form-Controls/form-group-options.board.tsx
@@ -1,23 +1,13 @@
 import { createBoard } from '@wixc3/react-board';
 import { FormGroup, H5, InputGroup, Intent, Switch } from "@blueprintjs/core";
 // board cover
-import cover from '../../../../assets/form-controls/form-group1.svg';
+import cover from '../../../../assets/form-controls/form-group2.svg';
 
 export default createBoard({
     name: 'Form Group Options',
     Board: () => {
         return (
             <div>
-                <FormGroup
-                    disabled={false}
-                    intent="none"
-                    label={"Label"}
-                    labelFor="text-input"
-                    subLabel={"Label helper text with details..."}
-                >
-                    <InputGroup id="text-input" placeholder="Placeholder text" intent={'none'} />
-                </FormGroup>
-
                 <FormGroup
                     disabled={false}
                     intent="none"
