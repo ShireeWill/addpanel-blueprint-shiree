cfa31f66c5c17ce33efdd0fd9a8eff6360653198
ShireeWill <shiree@wix.com>
2023-06-15 16:24:44 +0300

    Spinners cover image

--- src/_codux/boards/Blueprint/Spinners/primary.board.tsx
+++ src/_codux/boards/Blueprint/Spinners/primary.board.tsx
@@ -2,6 +2,9 @@ import { createBoard } from '@wixc3/react-board';
 import { Spinner, ISpinnerProps } from '@blueprintjs/core';
 
 
+// board cover
+import cover from '../../../../../src/assets/spinners/primary.svg';
+
 export default createBoard({
     name: 'Primary',
     Board: () => 
